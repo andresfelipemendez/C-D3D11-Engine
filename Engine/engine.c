969339ff690db7ee8832e68f88e7efc9d23d33ac
Andres Felipe Mendez <fmendez1212@gmail.com>
2020-05-30 17:48:49 -0500

    share methods between dll and exe

--- Engine/engine.c
+++ Engine/engine.c
@@ -8,3 +8,26 @@ vector3 add(vector3 a, vector3 b)
     r.z = a.z + b.z;
     return r;
 }
+
+void_pGameMemory(Start) {
+
+    Print = memory->Print;
+    Print("hi");
+
+   /* renderComponents = malloc(sizeof(RenderComponent)* 512);
+    numRenderComponents = 0;*/
+
+    //Add Entity
+    unsigned int indices[] = {0, 1, 2};
+    void* iv = memory->CreateIndexBuffer(indices, 3);
+    //renderComponents[numRenderComponents].indices = iv;
+    
+
+    SimpleVertexCombined verticesCombo[3] = {
+        {{0.0f, 0.5f, 0.0f}, {0.0f, 0.0f, 0.5f}},
+        {{0.5f, -0.5f, 0.0f}, {0.5f, 0.0f, 0.5f}},
+        {{-0.5f, -0.5f, 0.0f}, {0.0f, 0.5f, 0.5f}}
+    };
+    
+    //registerSystem(&renderSystem);
+}
--- Engine/engine.h
+++ Engine/engine.h
@@ -1,14 +1,8 @@
 #pragma once
+//#include <cstddef>
 
 #define DllExport __declspec(dllexport)
-
 #define PI (3.14159265358979323846)
-
-typedef struct
-{
-    float X, Y, Z;
-    float color[4];
-} VERTEX;
 
 typedef struct {
     float x, y ,z;
@@ -19,33 +13,20 @@ typedef struct {
     float Col[3];
 } SimpleVertexCombined;
 
-
 DllExport vector3 add(vector3 a, vector3 b);
 
-#define SET_CLEAR_COLOR(name) void name(float *color)
-typedef SET_CLEAR_COLOR(set_clear_color);
-
-#define SET_WORLD_VIEW_PROJECTION_MATRIX(name) void name(vector3 up, vector3 position, vector3 lookAt, vector3 rot)
-typedef SET_WORLD_VIEW_PROJECTION_MATRIX(set_world_view_projection_matrix);
-
-#define SET_VERTEX_BUFFER(name) void name(unsigned int *stride, unsigned int *offset)
-typedef SET_VERTEX_BUFFER(set_vertex_buffer);
-
 #define pVoid_pUIntUint(name) void* name(unsigned int *stride, unsigned int size)
-
 typedef pVoid_pUIntUint(create_index_buffer);
 
-#define DRAW(name) void name(void)
-typedef DRAW(draw);
-
-#define DEBUG_PLATFORM_PRINT_CONSOLE(name) void name(char *message)
-typedef DEBUG_PLATFORM_PRINT_CONSOLE(debug_platform_print_console);
+#define void_pChar(name) void name(char *message)
+typedef void_pChar(print);
+print *Print;
 
 typedef struct {
-    debug_platform_print_console *DEBUGPlatformPrintConsole;
-    set_clear_color *SETClearColor;
-    set_world_view_projection_matrix *SETWorldViewProjectionMatrix;
-    set_vertex_buffer *SETVertexBuffer;
     create_index_buffer *CreateIndexBuffer;
-    draw *Draw;
+    print *Print;
 } GameMemory;
+
+#define void_pGameMemory(name) DllExport void name(GameMemory* memory)
+typedef void_pGameMemory(start);
+typedef void_pGameMemory(update);
