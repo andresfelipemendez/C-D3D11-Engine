7fe7531902ad68f78993c8c2fcbe8803af2b23cf
Andres Felipe Mendez <fmendez1212@gmail.com>
2020-06-02 10:42:43 -0500

    entity manager to handle archetypes and components that go toghether

--- Engine/RenderingComponent.c
+++ Engine/RenderingComponent.c
@@ -1,19 +1,2 @@
 #include "RenderingComponent.h"
-#include "engine.h"
 
-static unsigned int nrcs = 0;
-unsigned int* pNumRenderingComponents = &nrcs;
-
-static RenderingComponent renderingComponents[512];
-RenderingComponent* pRenderingComponents= renderingComponents;
-
-unsigned int CreateRenderingComponent(
-	unsigned int* indices, unsigned int indicesLength, 
-	SimpleVertexCombined* vertices, unsigned int verticesLength)
-{
-	renderingComponents[nrcs].numIndices = indicesLength;
-	renderingComponents[nrcs].pIndexBuffer = createIndexBuffer(indices, indicesLength);
-	renderingComponents[nrcs].pVertexBuffer = createVertexBuffer(vertices, verticesLength);
-	nrcs++;
-    return 0;
-}
--- Engine/engine.c
+++ Engine/engine.c
@@ -1,4 +1,5 @@
 #include "engine.h"
+#include "EntityManager.h"
 #include "RenderingSystem.h"
 
 vector3 add(vector3 a, vector3 b)
@@ -24,29 +25,38 @@ void_pGameMemory(Start) {
     pSetBuffers = malloc(sizeof(void*));
     pSetBuffers = memory->SetBuffers;
 
+    InitEntityManager();
     initSystemManager();
+
+
+	//createEntity()
+	unsigned int entity = createEntityArchetiypeRendererTraslationInput();
 
     //traslation t1 = {0};
     unsigned int indices[] = {3, 1, 0, 0, 1, 2};
-	SimpleVertexCombined verticesCombo[] = {
+	SimpleVertexCombined vertices[] = {
 		{{-1.0f,  1.0f, 0.0f}, {1.0f, 1.0f, 1.0f}},
 		{{-0.9f, -1.0f, 0.0f}, {1.0f, 1.0f, 1.0f}},
 		{{-1.0f, -1.0f, 0.0f}, {1.0f, 1.0f, 1.0f}},
         {{-0.9f,  1.0f, 0.0f}, {1.0f, 1.0f, 1.0f}}
 	};
+    archetypeRendererTraslationInput->renderingComponents[entity].pIndexBuffer = createIndexBuffer(indices, NELEMS(indices));
+    archetypeRendererTraslationInput->renderingComponents[entity].pVertexBuffer = createVertexBuffer(vertices, NELEMS(vertices));
 
-	CreateRenderingComponent(indices, NELEMS(indices), verticesCombo, NELEMS(verticesCombo));
+
 
     //traslation t2 = {0};
     unsigned int indices2[] = {1, 3, 0, 1, 0, 2};
-	SimpleVertexCombined verticesCombo2[] = {
+	SimpleVertexCombined vertices2[] = {
 		{{1.0f,  1.0f, 0.0f}, {1.0f, 1.0f, 1.0f}},
 		{{0.9f, -1.0f, 0.0f}, {1.0f, 1.0f, 1.0f}},
 		{{1.0f, -1.0f, 0.0f}, {1.0f, 1.0f, 1.0f}},
 		{{0.9f,  1.0f, 0.0f}, {1.0f, 1.0f, 1.0f}}
 	};
 
-    CreateRenderingComponent(indices2, NELEMS(indices2), verticesCombo2, NELEMS(verticesCombo2));
+	unsigned int entity2 = createEntityArchetiypeRendererTraslationInput();
+	archetypeRendererTraslationInput->renderingComponents[entity2].pIndexBuffer = createIndexBuffer(indices2, NELEMS(indices2));
+	archetypeRendererTraslationInput->renderingComponents[entity2].pVertexBuffer = createVertexBuffer(vertices2, NELEMS(vertices2));
 
     registerSystem(renderingSystem);
 }
