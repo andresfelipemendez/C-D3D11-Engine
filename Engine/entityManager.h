53bc6e0bc0148cde27866115d83e7cb201777aae
Andres Felipe Mendez <fmendez1212@gmail.com>
2020-06-02 09:33:53 -0500

    Beginning to think in SOA

--- Engine/entityManager.h
+++ Engine/entityManager.h
@@ -1,4 +1,18 @@
 #pragma once
 
-void createEntity();
+#include "RenderingComponent.h"
+#include "TraslationComponent.h"
+#include "InputComponent.h"
+
+// SOA or AOS? this is a SOA but I'm not sure if it is what the DOD dictates
+typedef struct {
+	int* entityId;
+	RenderingComponent* renderingComponents;
+	TraslationComponent* traslationComponents;
+	InputComponent* inputComponents;
+} ArchetypeRendererTraslationInput;
+
+ArchetypeRendererTraslationInput* archetypeRendererTraslationInput = 0;
+
+void* createEntityArchetiypeRendererTraslationInput();
 
